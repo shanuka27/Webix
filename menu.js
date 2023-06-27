14f78f3a43d534b2f515af1ecf2b55219b3dc253
shanuka27 <shanukayasiru27@gmail.com>
2023-06-27 23:21:34 +0530

    initial commit

---
+++ menu.js
@@ -0,0 +1,18 @@
+var menu_data = [
+    { id: "dashboard", icon: "dashboard", value: "Dashboard", data:[
+        { id: "subitem", icon: "shopping-cart", value: "charts"},
+    ]},
+    { id: "orders", icon: "shopping-cart", value: "Orders"},
+    { id: "products", icon: "coffee", value: "Products"},
+    { id: "users", icon: "users", value: "Users"},
+    { id: "settings", icon: "cogs", value: "Settings"},
+]
+
+var multiview_cells =[
+    { id: "dashboard", template: "dashboard"},
+    { id: "subitem", template: "subitem"},
+    { id: "orders", template: "Orders"},
+    { id: "products", template: "Products"},
+    { id: "users", template: "Users"},
+    { id: "settings", template: "Settings"},
+]
